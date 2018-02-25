d3fe92417becd5a46dfa5ed1154fc9b62400186b
Poulin-L <laurent.poulin420@gfi.fr>
2018-02-25 18:33:29 +0100

    removing logs and adding comments

--- inlinedropdown.js
+++ inlinedropdown.js
@@ -1,24 +1,27 @@
 $( document ).ready(function() {
     var upper = $('.uppernav');
     var childrens = upper.children();
-
+    //Verifying all elements of main menu
     for(var cpt = 0 ; cpt < childrens.length ; cpt++){
         let id = childrens[cpt].id;
-
+        //adding onclick action on It
         $('#'+id).click(function(){
             $(".lowernav").hide();
 
+            //if he has dropdowns
             if($('#'+id).hasClass("has-child")){
-                console.log('#lower'+id+" click");
 
+                //If already active
                 if($('#'+id).hasClass("upper-active")){
                     $(".main-nav ").removeClass("upper-active");
+                //If not
                 }else{
                     $(".main-nav ").removeClass("upper-active");
                     $('#lower'+id).show();
                     $('#'+id).addClass("upper-active");
                 }
-
+            
+            //If it doesn't have any dropdowns
             }else{
                 $(".main-nav ").removeClass("upper-active");
                 $('#'+id).addClass("upper-active");
